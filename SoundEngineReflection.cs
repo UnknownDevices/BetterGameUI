4d1a47da02da1e4f8b480d4688ed3ed457a03bac
UnknownDevices <94582306+UnknownDevices@users.noreply.github.com>
2022-11-13 13:17:42 -0600

    Fixed AmbiguousMatch exception being thrown from static constructor

--- SoundEngineReflection.cs
+++ SoundEngineReflection.cs
@@ -6,19 +6,15 @@ namespace BetterGameUI
 {
     public static class SoundEngineReflection
     {
-        public delegate void PlaySoundDelegate(
-            int type, int x = -1, int y = -1, int Style = 1, float volumeScale = 1f, float pitchOffset = 0f);
-
-        public static readonly PlaySoundDelegate PlaySound;
+        public static readonly MethodInfo PlaySoundInfo;
+        public static void PlaySound(int type, int x = -1, int y = -1, int Style = 1, 
+            float volumeScale = 1f, float pitchOffset = 0f) => 
+            PlaySoundInfo.Invoke(null, new object[] { type, x, y, Style, volumeScale, pitchOffset });
 
         static SoundEngineReflection() {
-            try {
-                PlaySound = typeof(SoundEngine).
-                GetMethod("PlaySound", BindingFlags.NonPublic | BindingFlags.Static).
-                CreateDelegate(typeof(PlaySoundDelegate)) as PlaySoundDelegate;
-            } catch {
-                Main.NewText("LOL");
-            }
+            PlaySoundInfo = typeof(SoundEngine).
+            GetMethod("PlaySound", 
+            BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly, new[] { typeof(int), typeof(int), typeof(int), typeof(int), typeof(float), typeof(float)});
         }
     }
 }
